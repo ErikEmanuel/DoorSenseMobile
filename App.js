bdf9d8f82fa789a239b3171342881964bde82934
joaopedrojesus1 <joaopedrojesus331@gmail.com>
2023-12-08 16:01:28 -0300

    Alteração dos inputs

--- App.js
+++ App.js
@@ -1,37 +1,64 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
-
   const [usuario, setUsuario] = useState('');
   const [senha, setSenha] = useState('');
   const [confirmar, setConfirmar] = useState('');
 
   const Criar = () => {
     alert('Usuário Criado');
-
   }
 
   return (
     <View style={styles.container}>
-        <StatusBar hidden />
+      <StatusBar hidden />
 
-        <Image style={styles.Logo} source={require('./assets/DoorSenseLogo.png')}/>
-        
-        <Text style={styles.title}>Bem-Vindo</Text>
-        <Text style={styles.Mensagem}>Primeiro acesso? Crie seu login e senha!</Text>
+      <Image style={styles.Logo} source={require('./assets/DoorSenseLogo.png')} />
 
-        <TextInput placeholder='Crie seu Usuario' style={styles.TextInput}onChangeText={Text=>setUsuario} />
-        <TextInput secureTextEntry = {true} placeholder='Crie uma senha' style={styles.TextInput}onChangeText={Text=>setSenha} />
-        <TextInput secureTextEntry = {true} placeholder='Confirme a senha' style={styles.TextInput}onChangeText={Text=>setConfirmar} />
+      <Text style={styles.title}>Bem-Vindo</Text>
+      <Text style={styles.Mensagem}>Primeiro acesso? Crie seu login e senha!</Text>
 
-        <TouchableOpacity style={styles.btnCriar} onPress={()=>Criar}>
-          <Text style={{color:'white', textAlign:'center'}}Criar>Criar</Text>
-        </TouchableOpacity>
+      <View style={styles.inputContainer}>
+        <View style={styles.inputWrapper}>
+          <Icon name="user" size={20} color="#05AFF2" style={styles.icon} />
+          <TextInput
+            placeholder='Crie seu Usuario'
+            style={styles.TextInput}
+            onChangeText={text => setUsuario(text)}
+          />
+        </View>
+        <View style={styles.Linha} />
 
-        
+        <View style={styles.inputWrapper}>
+          <Icon name="lock" size={20} color="#05AFF2" style={styles.icon} />
+          <TextInput
+            secureTextEntry={true}
+            placeholder='Crie uma senha'
+            style={styles.TextInput}
+            onChangeText={text => setSenha(text)}
+          />
+        </View>
+        <View style={styles.Linha} />
 
+        <View style={styles.inputWrapper}>
+          <Icon name="lock" size={20} color="#05AFF2" style={styles.icon} />
+          <TextInput
+            secureTextEntry={true}
+            placeholder='Confirme a senha'
+            style={styles.TextInput}
+            onChangeText={text => setConfirmar(text)}
+          />
+        </View>
+        <View style={styles.Linha} />
+      </View>
+
+      <TouchableOpacity style={styles.btnCriar} onPress={() => Criar()}>
+        <Text style={{ color: 'white', textAlign: 'center' }}>Criar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -42,44 +69,51 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20
+    padding: 20,
   },
-  Logo:{
-      width: '45%',
-      height: '20%',
-      marginTop: '-100%',
-      
-    },
-    title:{
-      fontWeight: 'bold',
-      color: '#05AFF2',
-      fontSize: 52,
-    },
-
-    Mensagem:{
-      fontSize: 20,
-      fontWeight: 'light'
-    },
-      
-    TextInput:{
-    width:'100%',
-    heigh: '40%',
+  Logo: {
+    width: '45%',
+    height: '20%',
+    marginBottom: 20,
+  },
+  title: {
+    fontWeight: 'bold',
+    color: '#05AFF2',
+    fontSize: 52,
+  },
+  Mensagem: {
+    fontSize: 15,
+    fontWeight: 'light',
+    marginBottom: 40,
+  },
+  inputContainer: {
+    width: '100%',
+  },
+  inputWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    marginRight: 10,
+  },
+  TextInput: {
     backgroundColor: 'white',
     borderRadius: 20,
-    paddingLeft:10,
-    marginBottom:10,
-    textAlign: 'center'
+    paddingLeft: 10,
+
   },
-  
-  btnCriar:{
+  Linha: {
+    borderBottomWidth: 2,
+    borderBottomColor: '#05AFF2',
+    width: '100%',
+    marginBottom: 25
+  },
+  btnCriar: {
     width: '30%',
     height: 40,
     backgroundColor: '#05AFF2',
     borderRadius: 5,
-    justifyContent:'center',
-    
+    justifyContent: 'center',
+    marginTop: 10,
   },
-  
-
-
 });
